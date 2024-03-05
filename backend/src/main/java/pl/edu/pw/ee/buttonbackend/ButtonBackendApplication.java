8430d60f2f02c8b4aaa4aeb19590ad2c91911c8a
KubaKrzych <krzychowiec.k@gmail.com>
2024-03-05 09:47:05 +0100

    Init backend

---
+++ backend/src/main/java/pl/edu/pw/ee/buttonbackend/ButtonBackendApplication.java
@@ -0,0 +1,14 @@
+package pl.edu.pw.ee.buttonbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class ButtonBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ButtonBackendApplication.class, args);
+	}
+
+}
